f2c39cd78e135123408a91f60e86d9165789e342
zhangxin <zhangxin@xiaozhu.com>
2018-04-24 20:04:49 +0800

    Initial commit

---
+++ Example/XZTestPodLib/xzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xzAppDelegate.h
+//  XZTestPodLib
+//
+//  Created by flymanzx on 04/24/2018.
+//  Copyright (c) 2018 flymanzx. All rights reserved.
+//
+
+@import UIKit;
+
+@interface xzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XZTestPodLib/xzViewController.h
@@ -0,0 +1,13 @@
+//
+//  xzViewController.h
+//  XZTestPodLib
+//
+//  Created by flymanzx on 04/24/2018.
+//  Copyright (c) 2018 flymanzx. All rights reserved.
+//
+
+@import UIKit;
+
+@interface xzViewController : UIViewController
+
+@end
